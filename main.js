af12c8097be970605800e0bd316d9086054f9865
Adrian <adrianh.macairan@gmail.com>
2019-02-21 20:45:04 -0500

    Created html, css, javascript

---
+++ main.js
@@ -0,0 +1,7 @@
+let yourName = prompt('What is your name?')
+let faveSport = prompt('What is your sport?')
+let faveFood = prompt('What is your favorite food?')
+
+confirm(`${yourName} your favorite movie is ${faveSport} and you love to eat ${faveFood}`)
+
+console.log(`${yourName} your sport is ${faveSport} and you love to eat ${faveFood}`)
